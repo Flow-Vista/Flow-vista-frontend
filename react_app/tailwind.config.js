4dc44b530bce0d422db040996dd5c50cdf887dd4
sandhiya <sandhiya.s@prowesstics.com>
2025-04-04 14:53:13 +0530

    packageui

--- react_app/tailwind.config.js
+++ react_app/tailwind.config.js
@@ -1,20 +1,11 @@
-// module.exports = {
-//   purge: [],
-//   darkMode: false, // or 'media' or 'class'
-//   theme: {
-//     extend: {},
-//   },
-//   variants: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx,css}"    // ✅ Ensure all relevant file types are included
+  ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
--- react_app/vite.config.js
+++ react_app/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import postcss from '@tailwindcss/postcss';  
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [
+        postcss,           
+        tailwindcss,
+        autoprefixer
+      ],
+    },
+    optimizeDeps: {
+      include: ['jwt-decode'], 
+    },
+  },
+});
